660e489402e4d2021da0301bf932123077854645
adam <atomicpenguines@gmail.com>
2018-11-22 21:11:26 +0000

    done

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 // import redux
+// connect also injects the dispatch function as a prop
 import { connect } from "react-redux";
 
 class Counter extends React.Component {
-  increment = () => {};
+  increment = () => {
+    this.props.dispatch({
+      type: "INCREMENT"
+    });
+  };
 
-  decrement = () => {};
+  decrement = () => {
+    this.props.dispatch({
+      type: "DECREMENT"
+    });
+  };
 
   render() {
     return (
@@ -22,6 +31,7 @@ class Counter extends React.Component {
   }
 }
 
+// really does just map state into props for the component to use
 function mapStateToProps(state) {
   return {
     count: state.count
@@ -33,4 +43,6 @@ function mapStateToProps(state) {
 // It’s written this way because connect is a higher-order function, it returns a function when you call it. And then calling that function with a component returns a new (wrapped) component.
 export default connect(mapStateToProps)(Counter);
 
-// What connect does is hook into Redux, pull out the entire state, and pass it through the mapStateToProps function that you provide. This needs to be a custom function because only you will know the “shape” of the state in Redux.
+// What connect does is hook into Redux, pull out the entire state, and pass it through the mapStateToProps function that you provide.
+
+// This needs to be a custom function because only you will know the “shape” of the state in Redux.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { render } from "react-dom";
 import Counter from "./Counter";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  console.log(state);
+  // just gonna leave this blank for now
+  // which is the same as `return undefined;`
+
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    //   Redux can (will) call your reducer with actions that it doesn’t know what to do with. In fact, the very first action you’ll receive is { type: "@@redux/INIT" }
+
+    // so a default state is needed
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById("root"));
